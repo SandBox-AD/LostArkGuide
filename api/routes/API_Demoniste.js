ca28618bfbb268121a9e3fec4f79a71611ea8468
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-02 23:15:18 +0100

    edit api

--- api/routes/API_Demoniste.js
+++ api/routes/API_Demoniste.js
@@ -5,6 +5,16 @@ const translate = require('translate');
 const translateFR = require('translate');
 const puppeteer = require('puppeteer');
 
+
+function processText(inputText) {
+    var output = [];
+    var json = inputText.split(' ');
+    json.forEach(function (item) {
+        output.push(item.replace(/\'/g, '').split(/(\d+)/).filter(Boolean));
+    });
+    return output;
+}
+
 async function scrap() {
     const brower = await puppeteer.launch({
         headless: true
@@ -13,7 +23,10 @@ async function scrap() {
     await page.goto("https://lost-ark.maxroll.gg/build-guides/demonic-impulse-shadowhunter-raid-guide");
     const tables = await page.evaluate(async () => {
         let data = {
-            "skills": [],
+            "skills": {
+                "name": [],
+                "level":[],
+            },
             "gameplayEn": [],
             "stats": [],
             "Engravings": {
@@ -44,12 +57,18 @@ async function scrap() {
                     "Effect": [],
                 },
             },
-
+            "Error":[]
         };
+
         try {
-            
-            document.querySelector("#advgb-col-5a1be502-f5c8-4961-b398-4fbf8a6b29a1 > div > figure > div > div > div > div > div > div.lap-body > div.lap-skills").innerText.split("\n").forEach(element => {
-                data['skills'].push(element);
+            document.querySelector("#advgb-col-a2526fce-a0c3-4cc8-a21c-f16383fe89c2 > div > figure > div > div > div > div > div > div.lap-body > div.lap-skills").textContent.split(/(\d+)/).forEach(element => {
+                if (element.length > 0) {
+                    if (!element.match(/(\d+)/)) {
+                        data['skills']['name'].push(element)
+                    } else {
+                        data['skills']['level'].push(element)
+                    }
+                }
             });
             document.querySelector("#ftwp-postcontent > ul:nth-child(24)").innerText.split("\n").forEach(element => {
                 data['gameplayEn'].push(element);
@@ -60,7 +79,7 @@ async function scrap() {
             document.querySelector("#ftwp-postcontent > ul:nth-child(31)").innerText.split("\n").forEach(element => {
                 data["stats"].push(element);
             });
-            document.querySelector("#ftwp-postcontent > p:nth-child(32)").innerText.split("\n").forEach(element => {
+            document.querySelector("#ftwp-postcontent > p:nth-child(32)").innerText.replace(" \n", "\n").split("\n").forEach(element => {
                 data["stats"].push(element);
             });
             document.querySelector("#ftwp-postcontent > ul:nth-child(40)").innerText.split("\n").forEach(element => {
@@ -72,33 +91,34 @@ async function scrap() {
             document.querySelector("#ftwp-postcontent > ul:nth-child(45)").innerText.split("\n").forEach(element => {
                 data["Engravings"]["Advanced"].push(element);
             });
-            document.querySelector("#advgb-cols-a89fed4a-8f68-48d1-a11b-60a820ac16f7 > div > div").innerText.replace(/^\s+|\s+$/gm, '').split("\n").forEach(element => {
-                data["Gear Sets"]["Tier1"].push(element);
+            document.querySelector("#advgb-cols-9901f6c9-e6a1-493b-8b7a-5d237a4043a8 > div > div").textContent.replace(/^\s+|\s+$/gm, ',').split(",").forEach(element => {
+                if (element.length > 0) {
+                    data["Gear Sets"]["Tier1"].push(element); 
+                }
             });
-            document.querySelector("#advgb-cols-42664f5e-da06-44bb-9587-b7658cddc369 > div > div").innerText.replace(/^\s+|\s+$/gm, '').split("\n").forEach(element => {
-                data["Gear Sets"]["Tier2"].push(element);
+            document.querySelector("#advgb-cols-403265ae-2b09-4bd9-a713-343f18d43b1c > div > div").textContent.replace(/^\s+|\s+$/gm, ',').split(",").forEach(element => {
+                if (element.length > 0) {
+                    data["Gear Sets"]["Tier2"].push(element);
+                }
             });
-            document.querySelector("#advgb-cols-3ec18506-2ab0-436b-9a46-0e6cece97f6f > div > div").innerText.replace(/^\s+|\s+$/gm, '').split("\n").forEach(element => {
-                data["Gear Sets"]["Tier3"].push(element);
+            document.querySelector("#advgb-cols-746ee974-5fdd-444e-af4c-db20e6e31753 > div > div").textContent.replace(/^\s+|\s+$/gm, ',').split(",").forEach(element => {
+                if (element.length > 0) {
+                    data["Gear Sets"]["Tier3"].push(element);
+                }
             });
-            document.querySelector("#advgb-col-09a8107f-852f-42cf-8bd9-cdeeacea61be > div > table").innerText.replace(/^\s+|\s+$/gm, '').replace('\t', '\n').split("\n").forEach((element, index) => {
+            document.querySelector("#advgb-col-eeb17e9e-2d0d-45d3-9961-f0b7a571195c > div > table > tbody").innerText.replace(/^\s+|\s+$/gm, '/').replace('\t', '\n').split("\n").forEach((element, index) => {
                 if (index >= 2) {
                     if (index % 2 == 0) {
-                        data["Runes"]["Skills"].push(element);
+                        data["Runes"]["Skills"].push(element.replace('/', ''));
     
                     } else {
-                        data["Runes"]["Runes"].push(element);
+                        data["Runes"]["Runes"].push(element.replaceAll('//',' / '));
                     }
                 }
             });
-            document.querySelector("#advgb-cols-bb1debb1-480b-48ec-b52f-a58a747661cc > div > div").innerText.split("\n").forEach((element, index) => {
-                if (index == 2) {
-                    data["Gems"]["Attacks"].push(element);
-                }
-                if (index == 6) {
-                    data["Gems"]["Cooldown"].push(element);
-                }
-            });
+            data['Gems']['Attacks'].push(document.querySelector("#advgb-col-c0cbd474-146a-43e0-a2b7-34fe910ec593 > div > p > span > span > span.lap-skill-name").innerText);
+            data['Gems']['Cooldown'].push(document.querySelector("#advgb-col-15c1bdd2-c5fd-4727-b4c9-0fc16de1c9bf > div > p > span > span > span.lap-skill-name").innerText);
+            
             document.querySelector("#ftwp-postcontent > figure:nth-child(78) > div > div > div > div").innerText.split("\n").forEach((element, index) => {
                 if (index <=5) {
                     data["Card Sets"]["Budget Card"]["Card"].push(element);
@@ -117,6 +137,7 @@ async function scrap() {
             });
         } catch (error) {
             console.log(error);
+            data.Error.push(error.message)
         }
         return data;
     });
