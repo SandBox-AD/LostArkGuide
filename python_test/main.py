0cdc3cc7711f16965772d6bd800c65dc4e9bf898
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-05 13:36:37 +0100

    edit test api python

--- python_test/main.py
+++ python_test/main.py
@@ -1,13 +1,19 @@
 import requests
 import json
 import logging
+import os
+import asyncio
+
+
+async def test():
+    for i in range(0,3000):
+        api = requests.get("http://localhost:9000/API/Demoniste")
+        if i == 0:
+            with open('response/demonisteTest.json', 'w') as outfile:
+                json.dump(api.json(), outfile)
+
 logging.basicConfig(filename='python_test/log/test.log', encoding='utf-8', level=logging.DEBUG)
-for i in range(0,3000):
-    api = requests.get("http://localhost:9000/API/Demoniste")
-    if i == 0:
-        with open('response/demonisteTest.json', 'w') as outfile:
-            json.dump(api.json(), outfile)
-    if api.status_code == 200:
-        logging.info(api.status_code)
-    else :
-        logging.warning(api.status_code)
+os.system("echo start")
+asyncio.run(test())
+asyncio.run(test())
+os.system("echo end")
