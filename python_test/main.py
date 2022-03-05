b91601862ec43880b126ba7c8fd803c5267ffcb3
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-05 13:27:18 +0100

    Test Api

--- python_test/main.py
+++ python_test/main.py
@@ -1,8 +1,13 @@
 import requests
 import json
-x = ['']
+import logging
+logging.basicConfig(filename='python_test/log/test.log', encoding='utf-8', level=logging.DEBUG)
 for i in range(0,3000):
+    api = requests.get("http://localhost:9000/API/Demoniste")
     if i == 0:
-        with open('json_data.json', 'w') as outfile:
-            json.dump(requests.get("http://localhost:9000/API/Demoniste").json(), outfile)
-    x.append(requests.get("http://localhost:9000/API/Demoniste").status_code)
+        with open('response/demonisteTest.json', 'w') as outfile:
+            json.dump(api.json(), outfile)
+    if api.status_code == 200:
+        logging.info(api.status_code)
+    else :
+        logging.warning(api.status_code)
